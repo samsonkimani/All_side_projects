b2f86c34d1616af842a12e0e9bd0ee6bc3a463e1
samsonkimani <samsonkimani43@gmail.com>
2023-05-26 12:26:01 +0300

    creating the main and auth blueprint and creating the app initialization function

---
+++ flask_auth_app/project/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = "your secret key"
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+    db.init_app(app)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
