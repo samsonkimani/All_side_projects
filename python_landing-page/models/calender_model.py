ba760926af81112e241653a3719dac7b73327b0c
samsonkimani <samsonkimani43@gmail.com>
2023-06-13 17:44:33 +0300

    creating the main product page

--- python_landing-page/models/calender_model.py
+++ python_landing-page/models/calender_model.py
@@ -2,17 +2,18 @@
 
 from models.basemodel import BaseModel
 import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey, DateTime
+from sqlalchemy import Column, String, ForeignKey, Date
 from sqlalchemy.orm import relationship
 
 class Calendar(BaseModel):
     __tablename__ = 'calendar'
 
     doctor_id = Column(String(36), ForeignKey('doctors.id'), nullable=False)
-    appointment_datetime = Column(DateTime, nullable=False)
+    appointment_date = Column(Date, nullable=False)
+    appointment_time = Column(String(255))
 
     doctor = relationship("Doctor", backref="calendar")
 
-    def __init__(self, doctor_id, appointment_datetime):
+    def __init__(self, doctor_id, appointment_time, appointment_date):
         self.doctor_id = doctor_id
         self.appointment_datetime = appointment_datetime
--- python_landing-page/static/js/scripts.js
+++ python_landing-page/static/js/scripts.js
@@ -1,12 +1,3 @@
-/*!
-* Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
-//
-// Scripts
-//
-
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
@@ -50,5 +41,9 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
+
+    $(function(){
+		$('#datetimepicker1').datepicker();
+	});
 
 });
