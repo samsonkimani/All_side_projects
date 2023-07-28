6f719cc78b078ac30397c3b86d24d00bcb3b81ab
samsonkimani <samsonkimani43@gmail.com>
2023-07-28 16:56:10 +0300

    added more quotes

--- alx-final-party/src/App.jsx
+++ alx-final-party/src/App.jsx
@@ -1,12 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import './App.css'
 
-const namesArray = ['"The greatest glory in living lies not in never falling, but in rising every time we fall." - Nelson Mandela',
-  '"The future belongs to those who believe in the beauty of their dreams." - Eleanor Roosevelt',
-  '"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill',
-  '"The only limit to our realization of tomorrow will be our doubts of today." - Franklin D. Roosevelt',
-  '"The way to get started is to quit talking and begin doing." - Walt Disney',
-  '"It is during our darkest moments that we must focus to see the light." - Aristotle'];
+const namesArray = ['The greatest glory in living lies not in never falling, but in rising every time we fall.',
+  'The future belongs to those who believe in the beauty of their dreams.',
+  'Success is not final, failure is not fatal: It is the courage to continue that counts.',
+  'The only limit to our realization of tomorrow will be our doubts of today.',
+  'The way to get started is to quit talking and begin doing.',
+  'It is during our darkest moments that we must focus to see the light.',
+    'You’re off to great places, today is your day. Your mountain is waiting, so get on your way.',
+  'You always pass failure on the way to succes.',
+  'Winning doesn’t always mean being first. Winning means you’re doing better than you’ve done before.',
+  'You’re braver than you believe, and stronger than you seem, and smarter than you think.',
+  'It always seems impossible until it is done.',
+  'When you can’t find the sunshine, be the sunshine!',
+  'Happiness is not a station you arrive at, but a manner of traveling.',
+  'It makes a big difference in your life when you stay positive.',
+  'You are never too old to set another goal or dream a new dream.',
+  'It’s not whether you get knocked down, it’s whether you get up.',
+  'It ain’t about how hard ya hit. It’s about how hard you can get hit and keep moving forward.',
+  'The struggle you’re in toad is developing the strength you need tomorrow.',
+  'The way I see it, if you want the rainbow, you gotta put up with the rain.',
+  'Make each day your masterpiece.',
+  'A truly happy person is one who can enjoy the scenery while on a detour.',
+  'Keep your face to the sunshine and you cannot see a shadow.',
+  'A positive attitude causes a chain reaction of positive thoughts, events and outcomes. It is a catalyst and it sparks extraordinary results.',
+  'Don’t forget to tell yourself positive things daily! You must love yourself internally to glow externally.',
+  'The harder the battle, the sweeter the victory.',
+  'Never say never because limits, like fears, are often just an illusion.',
+  'You miss 100% of the shots you don’t take.',
+  'Winners never quit and quitters never win.',
+  'You can’t put a limit on anything. The more you dream, the farther you get.'
+];
 
 
 function App() {
@@ -73,7 +97,7 @@ function App() {
         </div>
       </div>
       <div className='ownership'>
-        <p>website by Alx students</p>
+        <p>website by Ihub SE Learners</p>
       </div>
     </div>
   );
